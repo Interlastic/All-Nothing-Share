842648652341dab9036a0c03542b588039a91e67
Interlastic <achillecharra@gmail.com>
2025-08-14 04:07:46 +0200

    Enhanced Navigation++

--- server.py
+++ server.py
@@ -46,6 +46,45 @@ def stream_items():
                 time.sleep(0.09)
     return Response(generate(), mimetype='application/x-ndjson')
 
+@app.route('/Phone<phone_id>')
+def serve_phone(phone_id):
+    # read our template file
+    with open(os.path.join(STATIC_FOLDER, 'templates/phonetemplate.html'), 'r') as f:
+        template = f.read()
+    
+    # replace placeholders based on phone_id
+    phone_data = get_phone_data(phone_id)  # ur function to get data
+    
+    html = template.replace('{{PHONE_NAME}}', phone_data['name'])
+    
+    #  Now we replace everything for otherphone
+    for i in range(1, 5):
+        other_phone_key = f'otherphone{i}'
+        if other_phone_key in phone_data:
+            html = html.replace(f'{{{{OTHER_PHONE{i}}}}}', phone_data[other_phone_key])
+        else:
+            html = html.replace(f'{{{{OTHER_PHONE{i}}}}}', 'Unknown')
+    # We also need to replace the "href"s so we need to take otherphone{i} like before but remove spaces. so if otherphone1 is Phone 2 we write Phone2 .
+    for i in range(1, 5):
+        other_phone_key = f'otherphone{i}'
+        if other_phone_key in phone_data:
+            phone_name = phone_data[other_phone_key]
+            phone_href = phone_name.replace(' ', '')
+            html = html.replace(f'{{{{OTHERPHONE{i}_HREF}}}}', f'/{phone_href}')
+
+    return html
+
+def get_phone_data(phone_id):
+    # return dict w phone info based on id
+    phones = {
+   '1': {'name': 'Phone 1', 'otherphone1': 'Phone 2', 'otherphone2': 'Phone 2a', 'otherphone3': 'Phone 3a', 'otherphone4': 'Phone 3', 'desc': 'test'},
+   '2': {'name': 'Phone 2', 'otherphone1': 'Phone 1', 'otherphone2': 'Phone 2a', 'otherphone3': 'Phone 3a', 'otherphone4': 'Phone 3', 'desc': 'test'},
+   '2a': {'name': 'Phone 2a', 'otherphone1': 'Phone 1', 'otherphone2': 'Phone 2', 'otherphone3': 'Phone 3a', 'otherphone4': 'Phone 3', 'desc': 'test'},
+   '3a': {'name': 'Phone 3a', 'otherphone1': 'Phone 1', 'otherphone2': 'Phone 2', 'otherphone3': 'Phone 2a', 'otherphone4': 'Phone 3', 'desc': 'test'},
+   '3': {'name': 'Phone 3', 'otherphone1': 'Phone 1', 'otherphone2': 'Phone 2', 'otherphone3': 'Phone 2a', 'otherphone4': 'Phone 3a', 'desc': 'test'},
+    }
+    return phones.get(phone_id, {'name': 'Unknown', 'desc': 'Not found'})
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
